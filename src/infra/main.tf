cd2ebab551ca389753caad1549f5c0add6d04d52
amlijupnandanan <67151561+amlijupnandanan@users.noreply.github.com>
2024-07-08 15:37:18 +0530

    Update main.tf

--- src/infra/main.tf
+++ src/infra/main.tf
@@ -1,20 +1,20 @@
-# module "resourcegroup" {
-#   for_each = local.configure_resource_groups.settings
-#   source   = "../modules/resource-group"
-#   settings = each.value
+ module "resourcegroup" {
+   for_each = local.configure_resource_groups.settings
+   source   = "../modules/resource-group"
+   settings = each.value
+ }
+# module "aks" {
+  # for_each = local.configure_aks_resources.settings
+  # source   = "../modules/aks"
+  # settings = each.value
 # }
-module "aks" {
-  for_each = local.configure_aks_resources.settings
-  source   = "../modules/aks"
-  settings = each.value
-}
-module "mssql" {
-  for_each = local.configure_mssql_resources.settings
-  source   = "../modules/mssql"
-  settings = each.value
-}
-module "acr" {
-  for_each = local.configure_acr_resources.settings
-  source   = "../modules/acr"
-  settings = each.value
-}
+# module "mssql" {
+  # for_each = local.configure_mssql_resources.settings
+  # source   = "../modules/mssql"
+  # settings = each.value
+# } 
+# module "acr" {
+  # for_each = local.configure_acr_resources.settings
+  # source   = "../modules/acr"
+  # settings = each.value
+# }
